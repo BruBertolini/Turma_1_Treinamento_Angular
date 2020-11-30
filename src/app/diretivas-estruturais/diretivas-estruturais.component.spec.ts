f284e64d85db1ab7610011d7572a1da85df7348c
Bertolini Correia <babo@gft.com>
2020-11-30 17:51:51 -0300

    Testes unitários

--- src/app/diretivas-estruturais/diretivas-estruturais.component.spec.ts
+++ src/app/diretivas-estruturais/diretivas-estruturais.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { DiretivasEstruturaisComponent } from './diretivas-estruturais.component';
 
@@ -8,9 +9,9 @@ describe('DiretivasEstruturaisComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DiretivasEstruturaisComponent ]
-    })
-    .compileComponents();
+      declarations: [DiretivasEstruturaisComponent],
+      imports: [RouterTestingModule],
+    }).compileComponents();
   });
 
   beforeEach(() => {
--- src/app/formularios/formularios.module.ts
+++ src/app/formularios/formularios.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TemplateDrivenComponent } from './template-driven/template-driven.component';
 import { ReactiveFormsComponent } from './reactive-forms/reactive-forms.component';
+import { MeuServicoService } from './meu-servico.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,11 +14,15 @@ import { ReactiveFormsComponent } from './reactive-forms/reactive-forms.componen
   imports: [
     CommonModule,
      FormsModule,
-     ReactiveFormsModule
+     ReactiveFormsModule,
+     HttpClientModule
     ],
   exports:[
     TemplateDrivenComponent,
     ReactiveFormsComponent
+  ],
+  providers:[
+    MeuServicoService
   ]
 })
 export class FormulariosModule {}
